9f6115cbf77eb573d7c2b4a08d874cd050396edb
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-02-06 12:51:49 +0800

    uploaded the data on the database

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def main():
 
 
 """
-Reads a txt file containing the source of the news and passes it as a parameter to call getContent function. 
-@return: void 
+Reads a txt file containing the source of the news and passes it as a parameter to call getContent function.
+@return: void
 @param: void
 """
 
@@ -34,10 +34,10 @@ def readSourceFile():
 
 
 """
-Calls the newsAPI to get the content and stores the different information associated 
-with the news articles. 
+Calls the newsAPI to get the content and stores the different information associated
+with the news articles.
 @return : news title, desciption, image link, content, polarity and subjectivity of content
-@param: source of the news from where you are extracting the content. eg. 'bbc-news' 
+@param: source of the news from where you are extracting the content. eg. 'bbc-news'
 """
 
 
@@ -53,9 +53,11 @@ def getContent(news_source):
     news_station = []
     news_author = []
     url_to_news = []
-    # Call the newsAPI
+
+   # Call the newsAPI
     news = newsapi.get_everything(
         sources=news_source, language='en', sort_by='relevancy', page=5)
+
     # Store the articles returned from the API call.
     articles = news['articles']
 
@@ -64,7 +66,8 @@ def getContent(news_source):
         version='2019-07-12', authenticator=authenticator)
     watson_nlu.set_service_url(config.IBM_URL)
 
-    for i in range(len(articles)):										# Loop through all the articles.
+    # Loop through all the articles.
+    for i in range(len(articles)):
         myarticles = articles[i]
         # Some articles have no content so need to check that.
         if (myarticles['content'] != None):
@@ -74,7 +77,7 @@ def getContent(news_source):
                 text=myarticles['content'], features={"sentiment": {}})
             watson_polarity = watson_sentiment.result['sentiment']['document']['score']
 
-            if (textblob_polarity > 0 and watson_polarity > 0):
+            if (textblob_polarity > 0.3 and watson_polarity > 0.3):
                 textblob_pol.append(textblob_polarity)
                 watson_pol.append(watson_polarity)
                 content.append(myarticles['content'])
@@ -94,16 +97,32 @@ def getContent(news_source):
 
 
 """
-Uploads the content of the news to the database. 
-@return: void 
+Uploads the content of the news to the database.
+@return: void
 @para: news title, desciption, image link, content, polarity and subjectivity of content
 """
 
 
-def postContent(title, desc, img, content,  textblob_polarity, watson_polarity, publish_date, news_station, news_author, url_to_news):
+def postContent(title, desc, url_to_img, content,  textblob_polarity, watson_polarity, publish_date, news_station, news_author, url_to_news):
+    firebase_app = firebase.FirebaseApplication(
+        'https://happynews-99c12.firebaseio.com/')
     for i in range(len(title)):
-        print(url_to_news[i])
-        #print(i, title[i], "\n DESCRIPTION: ", desc[i], "\n  Textblob: ", textblob_polarity[i], "  Watson: ", watson_polarity[i]," \n\n\n\n")
+
+        data = {
+            'Title': 'John Doe',
+            'Publish_date': publish_date[i],
+            'News_station': news_station[i],
+            'News_author': news_author[i],
+            'Description': desc[i],
+            'Url_to_img': url_to_img[i],
+            'Content': content[i],
+            'Url_to_news': url_to_news[i],
+            'Textblob_polarity': textblob_polarity[i],
+            'Watson_polarity': watson_polarity[i]
+
+        }
+        result = firebase_app.post('/happynews-99c12/', data)
+        print(result, " \n")
 
 
 if __name__ == '__main__':
