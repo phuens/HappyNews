a79dceda778ae0c1265d8b3727ba8a8206d50215
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-02-06 17:38:31 +0800

    making flask index file

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-def fetchContentDatabase():
-    home = []
+from flask import Flask, render_template
+from firebase import firebase
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Index():
     firebase_app = firebase.FirebaseApplication(
         'https://happynews-99c12.firebaseio.com/')
     content = firebase_app.get('/happynews-99c12/', '')
 
     for key, val in content.items():
-        print("key: ", key, "\n  Value: ",  val, "\n\n\n\n\n")
+        return render_template('index.html', title=val['Description'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
