cb5f961f02ea593e6f0d1f76f2e8b9e7ab640ee7
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-02-06 12:06:34 +0800

    gathering all the information from thew newsAPI

--- app.py
+++ app.py
@@ -7,27 +7,30 @@ from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOpt
 import config
 
 
-
 newsapi = NewsApiClient(api_key=config.API_KEY)
 
 
-def main(): 
-	readSourceFile()
-	
+def main():
+    readSourceFile()
+
+
 """
 Reads a txt file containing the source of the news and passes it as a parameter to call getContent function. 
 @return: void 
 @param: void
 """
-def readSourceFile (): 
-	source_file = open("sources.txt", "r")		# Read the file containing the news sources.
-	sources = source_file.readlines()			# Read line by line.
-	source_file.close()							# Close. 
 
-	for i in range (len(sources)):				# call the getContent function one by one with the news source as parameter.
-		print(sources[i]) 
-		getContent(sources[i])
 
+def readSourceFile():
+    # Read the file containing the news sources.
+    source_file = open("sources.txt", "r")
+    sources = source_file.readlines()			# Read line by line.
+    source_file.close()							# Close.
+
+    # call the getContent function one by one with the news source as parameter.
+    for i in range(len(sources)):
+        print(sources[i])
+        getContent(sources[i])
 
 
 """
@@ -36,41 +39,58 @@ with the news articles.
 @return : news title, desciption, image link, content, polarity and subjectivity of content
 @param: source of the news from where you are extracting the content. eg. 'bbc-news' 
 """
-def getContent (news_source):
-
-	title = []
-	desc = []
-	img = []
-	content = []
-	polarity = []
-	# Call the newsAPI 
-	news =  newsapi.get_everything(sources=news_source,language='en',sort_by='relevancy', page=5)	
-	articles = news['articles']											# Store the articles returned from the API call.
-
-	authenticator = IAMAuthenticator(config.IBM_API_KEY)
-	watson_nlu = NaturalLanguageUnderstandingV1(version='2019-07-12',authenticator=authenticator)
-	watson_nlu.set_service_url(config.IBM_URL)
 
 
+def getContent(news_source):
 
-	for i in range(len(articles)):										# Loop through all the articles. 
-		myarticles = articles[i]
-		if (myarticles['content'] != None):								# Some articles have no content so need to check that. 
-			textblob_polarity = TextBlob(myarticles['content']).sentiment.polarity			# Check the sentiment of the news content. 
-			watson_sentiment = watson_nlu.analyze( text = myarticles['content'], features = {"sentiment": {}})
-			watson_polarity = watson_sentiment.result['sentiment']['document']['score']
+    title = []
+    desc = []
+    img = []
+    content = []
+    publish_date = []
+    textblob_pol = []
+    watson_pol = []
+    news_station = []
+    news_author = []
+    url_to_news = []
+    # Call the newsAPI
+    news = newsapi.get_everything(
+        sources=news_source, language='en', sort_by='relevancy', page=5)
+    # Store the articles returned from the API call.
+    articles = news['articles']
 
-			if (textblob_polarity > 0 and watson_polarity > 0):
-				polarity.append(textblob_polarity)
-				content.append(myarticles['content'])
-				title.append(myarticles['title'])				
-				desc.append(myarticles['description'])
-				img.append(myarticles['urlToImage'])
+    authenticator = IAMAuthenticator(config.IBM_API_KEY)
+    watson_nlu = NaturalLanguageUnderstandingV1(
+        version='2019-07-12', authenticator=authenticator)
+    watson_nlu.set_service_url(config.IBM_URL)
 
-		else: 
-			continue
-	postContent(title, desc, img, content,polarity)
+    for i in range(len(articles)):										# Loop through all the articles.
+        myarticles = articles[i]
+        # Some articles have no content so need to check that.
+        if (myarticles['content'] != None):
+            textblob_polarity = TextBlob(
+                myarticles['content']).sentiment.polarity
+            watson_sentiment = watson_nlu.analyze(
+                text=myarticles['content'], features={"sentiment": {}})
+            watson_polarity = watson_sentiment.result['sentiment']['document']['score']
 
+            if (textblob_polarity > 0 and watson_polarity > 0):
+                textblob_pol.append(textblob_polarity)
+                watson_pol.append(watson_polarity)
+                content.append(myarticles['content'])
+                title.append(myarticles['title'])
+                desc.append(myarticles['description'])
+                img.append(myarticles['urlToImage'])
+                publish_date.append(myarticles['publishedAt'])
+                news_station.append(myarticles['source'])
+                news_author.append(myarticles['author'])
+                url_to_news.append(myarticles['url'])
+
+        else:
+            continue
+
+    postContent(title, desc, img, content, textblob_pol,
+                watson_pol, publish_date, news_station, news_author, url_to_news)
 
 
 """
@@ -78,22 +98,13 @@ Uploads the content of the news to the database.
 @return: void 
 @para: news title, desciption, image link, content, polarity and subjectivity of content
 """
-def postContent(title, desc, img, content, polarity):
-	for i in range(len(title)): 
-		print (i , "  ", title[i], "\n")
-		
-   
-
-if __name__ == '__main__': 
-	main()
 
 
+def postContent(title, desc, img, content,  textblob_polarity, watson_polarity, publish_date, news_station, news_author, url_to_news):
+    for i in range(len(title)):
+        print(url_to_news[i])
+        #print(i, title[i], "\n DESCRIPTION: ", desc[i], "\n  Textblob: ", textblob_polarity[i], "  Watson: ", watson_polarity[i]," \n\n\n\n")
 
 
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
